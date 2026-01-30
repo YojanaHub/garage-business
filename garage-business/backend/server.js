ec29fdf490603b670a0a52987edf9a415e9776e6
YojanaHub <shreerangakg@gmail.com>
2026-01-30 18:41:40 +0530

    Update server.js

--- garage-business/backend/server.js
+++ garage-business/backend/server.js
@@ -1,1 +1,10 @@
-const express=require('express');const app=express();require('dotenv').config();app.get('/',(_,res)=>res.send('OK'));app.listen(4000);
+const cors = require("cors");const express=require('express');const app=express();app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://garage-business.vercel.app"
+    ],
+    credentials: true
+  })
+);
+require('dotenv').config();app.get('/',(_,res)=>res.send('OK'));app.listen(4000);
